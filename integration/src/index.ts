6efa54d5161b193554ab2d4f0a51c3b68673a8bb
Artem Zimin <atm.zmn95@gmail.com>
2020-02-27 13:41:27 +0300

    Add integration folder

---
+++ integration/src/index.ts
@@ -0,0 +1,7 @@
+import { Doctor } from "@hospital-sdk-trimmtrab/doctor";
+import { Patient } from "@hospital-sdk-trimmtrab/patient";
+import { Scheduler } from "@hospital-sdk-trimmtrab/scheduler";
+
+console.log(new Doctor());
+console.log(new Scheduler());
+console.log(new Patient());
